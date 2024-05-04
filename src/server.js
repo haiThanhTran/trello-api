1724169f8c996d6aa7cf1172009037e1fd20d535
haiThanhTran <tranthanhhaivlogs@gmail.com>
2024-05-04 21:14:07 +0700

    init_code

---
+++ src/server.js
@@ -0,0 +1,14 @@
+const express = require('express');
+
+const app = express();
+
+const hostname='localhost';
+const port=8817
+app.get("/", function (req, res) {
+  res.send("<h1>Hello World</h1>");
+});
+
+
+app.listen (port,hostname, () => {
+    console.log(`Hello haiThanhTran,tam running server at http://${hostname}:${port}/`);
+})
